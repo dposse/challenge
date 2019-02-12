f1172f5e5ec059e74f0568f3d2a4daf3d90d8dfc
dposse <dposse.125@gmail.com>
2019-02-12 10:19:35 -0500

    Problem 4 done

---
+++ problem4.js
@@ -0,0 +1,20 @@
+//Problem 4
+
+//given
+const array = [34, 203, 16, 46, 34, 432, 342, 124, 33, 188, 12];
+
+//find average
+//almost positive this can be done in a cool way with array.reduce(), but I would have to look it up as I don't remember exactly how it works
+
+let total = 0;
+
+array.forEach( (int) => {
+    
+    total += int;
+    console.log(int, total);
+    
+});
+
+console.log(total);
+
+alert("Average is " + (total / array.length));
